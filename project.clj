06956921ae27da31509d81bc87c5dac7d27195c6
Mike Callahan <chimpymike@gmail.com>
2017-06-06 16:56:47 -0400

    Add the apache maven http wagon dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.apache.maven.wagon/wagon-http "2.12"]]
   :eval-in-leiningen true)
