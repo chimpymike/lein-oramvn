9731654bbad4cb2e3fc8f781a0bbe2c8423d91a5
Mike Callahan <chimpymike@gmail.com>
2017-06-07 15:27:46 -0400

    Update project metadata for release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-oramvn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin that enables retrieving dependencies from the Oracle Maven Repository"
+  :url "https://github.com/chimpymike/lein-oramvn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.maven.wagon/wagon-http "2.12"]]
